b791e876914fb93d63d6fa459486c68a7d5fe19c
Regina <49993734+skrega@users.noreply.github.com>
2020-07-18 01:45:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.slider__items').slick({
+        fade: true,
+        infinity: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+      
+    });
+  
+});
